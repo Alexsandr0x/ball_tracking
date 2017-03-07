74f07b2e97d95f65d272fa7b7b083563334f42d4
Alexsandro <alexsandro2134@gmail.com>
2017-03-07 16:54:50 -0300

    Create ball tracking for image

---
+++ image_ball_finder.py
@@ -0,0 +1,39 @@
+import cv2
+import json
+import numpy as np
+
+file_directory = "config.json"
+try:
+    config_ = json.loads(open(file_directory).read())
+except Exception as error:
+    raise error
+
+colors = config_['colors']['ball_jpg']
+lower_color = np.array(colors[0])
+upper_color = np.array(colors[1])
+
+image_dir = config_["image_path"]
+
+picture = cv2.imread(image_dir)
+
+picture = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
+
+picture_hsv = cv2.cvtColor(picture, cv2.COLOR_BGR2HSV)
+
+mask = cv2.inRange(picture_hsv, lower_color, upper_color)
+
+im_orange = picture.copy()
+im_orange[mask == 0] = 0
+
+contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+if len(contours) > 0:
+    c = max(contours, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
+    M = cv2.moments(c)
+
+    if M["m00"] != 0:
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        cv2.circle(picture, center, 10, (0, 255, 0), -1)
+
+cv2.imwrite('frame.png', picture)
--- tests.py
+++ tests.py
@@ -14,12 +14,17 @@ except Exception as error:
 video_path = config_['video_path']
 cap = cv2.VideoCapture(video_path)
 
+fps = cap.get(cv2.CAP_PROP_FPS)
+
 colors = config_['colors']['ball']
-lower_blue = np.array(colors[0])
-upper_blue = np.array(colors[1])
+has_alien = config_.get('alien')
+lower_color = np.array(colors[0])
+upper_color = np.array(colors[1])
 counter = 0
+speed = None
 dx = None
 dy = None
+center = None
 
 point_buffer = 64
 points = deque(maxlen=point_buffer)
@@ -38,7 +43,10 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(frame, lower_blue, upper_blue)
+    mask = cv2.inRange(frame, lower_color, upper_color)
+
+    im_orange = frame.copy()
+    im_orange[mask == 0] = 0
 
     contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
@@ -63,20 +71,41 @@ while True:
             # compute the difference between the x and y
             # coordinates and re-initialize the direction
             # text variables
-            dx = points[-10][0] - points[i][0]
-            dy = points[-10][1] - points[i][1]
 
             set_x = [points[p_iter][0] for p_iter in range(0, 10)]
             set_y = [points[p_iter][1] for p_iter in range(0, 10)]
+
+            dx = points[-10][0] - points[i][0]
+            dy = points[-10][1] - points[i][1]
+
             z = np.polyfit(set_x, set_y, 1)
 
-            cv2.line(frame, get_points(z[0], z[1], 0), get_points(z[0], z[1], 1000), (255, 0, 0))
+            dir_x = -1 if np.sign(dx) == 1 else 1
+
+            cv2.line(frame, center, get_points(z[0], z[1], 1000 * dir_x), (255, 0, 0))
 
             break
 
+    if len(points) > fps:
+        speed = (np.array(points[0]) - np.array(points[1]))
+
     cv2.putText(frame, "dx: {}, dy: {}".format(dx, dy),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                0.35, (0, 0, 0), 1)
+                (0, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
+    cv2.putText(frame, "fps: {}".format(fps),
+                (0, 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
+    if center and has_alien:
+        s_img = cv2.imread("others/ufo.png")
+        s_img = cv2.resize(s_img, None, fx=0.125, fy=0.125, interpolation=cv2.INTER_CUBIC)
+        try:
+            offset_x = center[1]
+            offset_y = center[0]
+            frame[offset_x:offset_x + s_img.shape[0], offset_y:offset_y + s_img.shape[1]] = s_img
+        except:
+            pass
 
     cv2.imshow('frame', frame)
     counter += 1
