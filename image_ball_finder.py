b1fb917ce15b06fd0f97ba1cb77b5c9fc46c5ff9
Alexsandro <alexsandro2134@gmail.com>
2017-03-11 15:27:33 -0300

    Add pyCharm stuff in gitignore

--- image_ball_finder.py
+++ image_ball_finder.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import cv2
 import json
 import numpy as np
@@ -8,32 +10,63 @@ try:
 except Exception as error:
     raise error
 
+
+def find_ball(image_path, lower_color, upper_color, out_file='frame'):
+    """
+    Metodo que implementa todo o processo de encontrar a bola na imagem.
+    Abre uma janela com a imagem de image_path com o ponto e retorna coordenadas da
+    bola encontrada.
+
+    :param image_path: o caminho da imagem a ser processada
+    :param lower_color: limite inferior para cor que irá procurar em nparray(B, G, R)
+    :param upper_color: limite superior para cor que irá procurar em nparray(B, G, R)
+    :param out_file: arquivo de saida da imagem.
+    :return (coords, image): coordenadas (x, y) da bola encontrada e imagem aṕos processamento
+    """
+    center = None
+
+    picture = cv2.imread(image_path)
+
+    picture_no_noise = cv2.blur(picture, (5, 5))
+    # Podem haver casos onde a imagem estaja com muito ruido, nesse caso podemos usar
+    # ou um algoritmo simples de blur ou um algoritmo de "denoise" mais sofisticado
+    # nesse caso acabei usando o blur simples por ser mais rapido e ser o suficiente
+    # para proximo do centro da bola as predições.
+
+    # picture_no_noise = cv2.fastNlMeansDenoisingColored(picture, None, 15, 10, 7, 21)
+    # picture_no_noise = picture
+
+    picture_hsv = cv2.cvtColor(picture_no_noise, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(picture_hsv, lower_color, upper_color)
+
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        m = cv2.moments(c)
+
+        if m["m00"] != 0:
+            center = (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"]))
+            cv2.circle(picture_no_noise, center, 10, (0, 255, 0), -1)
+
+    image_file = 'out/{}.png'.format(out_file)
+    cv2.imwrite(image_file, picture_no_noise)
+
+    return center, picture_no_noise
+
 colors = config_['colors']['ball_jpg']
-lower_color = np.array(colors[0])
-upper_color = np.array(colors[1])
+l_color = np.array(colors[0])
+u_color = np.array(colors[1])
 
-image_dir = config_["image_path"]
+paths = config_["image_path"]
 
-picture = cv2.imread(image_dir)
-
-picture_no_noise = cv2.fastNlMeansDenoisingColored(picture, None, 15, 10, 7, 21)
-
-picture_hsv = cv2.cvtColor(picture_no_noise, cv2.COLOR_BGR2HSV)
-
-mask = cv2.inRange(picture_hsv, lower_color, upper_color)
-
-im_orange = picture_no_noise.copy()
-im_orange[mask == 0] = 0
-
-contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-
-if len(contours) > 0:
-    c = max(contours, key=cv2.contourArea)
-    ((x, y), radius) = cv2.minEnclosingCircle(c)
-    M = cv2.moments(c)
-
-    if M["m00"] != 0:
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        cv2.circle(picture_no_noise, center, 10, (0, 255, 0), -1)
-
-cv2.imwrite('out/frame.png', picture_no_noise)
+if isinstance(paths, list):
+    for i, path in enumerate(paths):
+        print find_ball(path, l_color, u_color, 'frame_{}'.format(i))[0]
+elif isinstance(paths, basestring):
+    print find_ball(paths, l_color, u_color)[0]
+else:
+    print '[ERROR]: \'image_path\' in config file must be string or list of strings!'
+    exit()
